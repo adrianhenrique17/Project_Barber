4ba7a7eb8327ebfde8b5b43e619c236b7413cbfb
adrianhenrique17 <hs8953064@gmail.com>
2025-08-26 17:05:09 -0300

    feat : adicionando rotas detalhes barber

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,70 @@
+import { db } from "@/app/_lib/prisma"
+import Image from "next/image"
+import { Button } from "@/app/_components/ui/button"
+import { ChevronLeftIcon, MenuIcon, MapPinIcon, StarIcon } from "lucide-react"
+import Link from "next/link"
+
+interface BarbershopPageProps {
+  params: Promise<{ id: string }>
+}
+
+const BarbershopPage = async ({ params }: BarbershopPageProps) => {
+  const { id } = await params
+
+  const barbershop = await db.barbershop.findUnique({
+    where: { id },
+  })
+
+  if (!barbershop) {
+    return <h1>Barbearia não encontrada</h1>
+  }
+
+  return (
+    <div>
+      <div className="relative h-[250px] w-full">
+        <Image src={barbershop?.imageUrl} fill className="object-cover" />
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute left-4 top-4"
+          asChild
+        >
+          <Link href="/">
+            <ChevronLeftIcon />
+          </Link>
+        </Button>
+
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute right-4 top-4"
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+      <div className="border-b border-solid p-5">
+        <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
+        <div className="mb-2 flex items-center gap-1">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-sm">{barbershop?.address} </p>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <StarIcon className="fill-primary text-primary" size={18} />
+          <p className="text-sm"> 5,0 (499 avaliações) </p>
+        </div>
+      </div>
+
+      <div>
+        <div className="space-y-3 border-b border-solid p-5">
+          <h2 className="text-xs font-bold uppercase text-gray-400">
+            Sobre nós
+          </h2>
+          <p className="text-sm">{barbershop?.description}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BarbershopPage
--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,12 @@ import Footer from "./_components/footer"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
+
   return (
     <div>
       <Header />
@@ -67,9 +73,25 @@ const Home = async () => {
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
 
-      <Footer />
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400"> ©2025 Copyright AdrianDev</p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
