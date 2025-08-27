015f29b050859e90fa2d8210a56b8bd897e1ebdd
adrianhenrique17 <hs8953064@gmail.com>
2025-08-26 21:41:55 -0300

    feat: adicionando elementod

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -3,16 +3,22 @@ import Image from "next/image"
 import { Button } from "@/app/_components/ui/button"
 import { ChevronLeftIcon, MenuIcon, MapPinIcon, StarIcon } from "lucide-react"
 import Link from "next/link"
+import ServiceItem from "@/app/_components/service-item"
 
 interface BarbershopPageProps {
-  params: Promise<{ id: string }>
+  params: { id: string }
 }
 
 const BarbershopPage = async ({ params }: BarbershopPageProps) => {
-  const { id } = await params
+  const { id } = params
 
   const barbershop = await db.barbershop.findUnique({
-    where: { id },
+    where: {
+      id,
+    },
+    include: {
+      services: true,
+    },
   })
 
   if (!barbershop) {
@@ -22,7 +28,12 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
   return (
     <div>
       <div className="relative h-[250px] w-full">
-        <Image src={barbershop?.imageUrl} fill className="object-cover" />
+        <Image
+          src={barbershop.imageUrl}
+          alt={barbershop.name}
+          fill
+          className="object-cover"
+        />
         <Button
           size="icon"
           variant="secondary"
@@ -42,16 +53,17 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           <MenuIcon />
         </Button>
       </div>
+
       <div className="border-b border-solid p-5">
         <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
         <div className="mb-2 flex items-center gap-1">
           <MapPinIcon className="text-primary" size={18} />
-          <p className="text-sm">{barbershop?.address} </p>
+          <p className="text-sm">{barbershop.address}</p>
         </div>
 
         <div className="flex items-center gap-1">
           <StarIcon className="fill-primary text-primary" size={18} />
-          <p className="text-sm"> 5,0 (499 avaliações) </p>
+          <p className="text-sm">5,0 (499 avaliações)</p>
         </div>
       </div>
 
@@ -60,8 +72,15 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           <h2 className="text-xs font-bold uppercase text-gray-400">
             Sobre nós
           </h2>
-          <p className="text-sm">{barbershop?.description}</p>
+          <p className="text-sm">{barbershop.description}</p>
         </div>
+      </div>
+
+      <div className="p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+        {barbershop.services.map((service) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
       </div>
     </div>
   )
