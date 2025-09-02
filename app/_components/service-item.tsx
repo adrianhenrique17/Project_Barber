097569c4f9645b863b26f529a17ab3951d4bea0f
adrianhenrique17 <hs8953064@gmail.com>
2025-09-02 17:16:16 -0300

    feat: barra lateral de reserva

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -1,13 +1,63 @@
+"use client"
+
 import { BarbershopService } from "@prisma/client"
 import Image from "next/image"
 import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
+import {
+  Sheet,
+  SheetTrigger,
+  SheetHeader,
+  SheetContent,
+  SheetTitle,
+} from "./ui/sheet"
+import { Calendar } from "./ui/calendar"
+import { ptBR } from "date-fns/locale"
+import { useState } from "react"
 
-interface ServiceItemProps {
+export interface ServiceItemProps {
   service: BarbershopService
+  barbershop: {
+    name: string
+  }
 }
 
-const ServiceItem = ({ service }: ServiceItemProps) => {
+const TIME_LIST = [
+  "08:00",
+  "08:30",
+  "09:00",
+  "09:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+  "17:30",
+  "18:00",
+]
+
+const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
+  const [selectedDay, setSelectDay] = useState<Date | undefined>(null)
+  const [selectTime, setSelectTime] = useState<string | undefined>(null)
+
+  const HandleDateSelect = (date: Date | undefined) => {
+    setSelectDay(date)
+  }
+
+  const handleTimeSelect = (time: string) => {
+    setSelectTime(time)
+  }
+
   return (
     <Card>
       <CardContent className="flex items-center gap-3 p-3">
@@ -16,6 +66,7 @@ const ServiceItem = ({ service }: ServiceItemProps) => {
             src={service.imageUrl}
             fill
             className="rounded-lg object-cover"
+            alt={service.name}
           />
         </div>
 
@@ -30,9 +81,77 @@ const ServiceItem = ({ service }: ServiceItemProps) => {
               }).format(Number(service.price))}
             </p>
 
-            <Button variant="secondary" size="sm">
-              Reservar
-            </Button>
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant="secondary" size="sm">
+                  Reservar
+                </Button>
+              </SheetTrigger>
+              <SheetContent className="px-0">
+                <SheetHeader>
+                  <SheetTitle>Fazer Reserva</SheetTitle>
+                </SheetHeader>
+
+                <div className="border-b border-solid p-5">
+                  <Calendar
+                    mode="single"
+                    locale={ptBR}
+                    selected={selectedDay}
+                    onSelect={HandleDateSelect}
+                    fromDate={new Date()}
+                    className="w-full"
+                  />
+                </div>
+
+                {selectedDay && (
+                  <div className="flex gap-3 overflow-x-auto border-b border-solid p-5 [&::-webkit-scrollbar]:hidden">
+                    {TIME_LIST.map((time) => (
+                      <Button
+                        key={time}
+                        variant={selectTime == time ? "default" : "outline"}
+                        className="rounded-full"
+                        onClick={() => handleTimeSelect(time)}
+                      >
+                        {time}
+                      </Button>
+                    ))}
+                  </div>
+                )}
+
+                {selectTime && (
+                  <div className="p-5">
+                    <div className="space-y-3">
+                      <div className="flex items-center justify-between">
+                        <h2 className="font-bold">{service.name}</h2>
+                        <p className="text-sm font-bold text-primary">
+                          {Intl.NumberFormat("pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                          }).format(Number(service.price))}
+                        </p>
+                      </div>
+
+                      <div className="flex items-center justify-between">
+                        <h2 className="text-sm text-gray-400">Data</h2>
+                        <p className="text-sm">
+                          {selectedDay.toLocaleDateString("pt-BR")}
+                        </p>
+                      </div>
+
+                      <div className="flex items-center justify-between">
+                        <h2 className="text-sm text-gray-400">Horário</h2>
+                        <p className="text-sm">{selectTime}</p>
+                      </div>
+
+                      <div className="flex items-center justify-between">
+                        <h2 className="text-sm text-gray-400">Barbearia</h2>
+                        <p className="text-sm">{barbershop.name}</p>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </SheetContent>
+            </Sheet>
           </div>
         </div>
       </CardContent>
